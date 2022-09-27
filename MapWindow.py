08aa44d556be1fbc4e785f764374401b9365ec31
PatateHunter <45663157+PatateHunter@users.noreply.github.com>
2022-09-27 21:53:35 +0200

    remove rocks

--- MapWindow.py
+++ MapWindow.py
@@ -2,12 +2,14 @@ import arcade
 
 import Consts
 
+
 class MapWindow(arcade.Window):
     def __init__(self, agent):
         super().__init__(agent.environment.width * Consts.SPRITE_SIZE,
                          agent.environment.height * Consts.SPRITE_SIZE,
                          'Gold Digger')
 
+        self.__rock_sprites = arcade.SpriteList()
         self.__agent = agent
         self.__iteration = 1
 
@@ -24,7 +26,9 @@ class MapWindow(arcade.Window):
                 sprite = arcade.Sprite(":resources:images/tiles/stone.png", 0.25)
 
                 sprite.center_x, sprite.center_y = self.state_to_xy(state)
-                self.__walls.append(sprite)
+                #self.__walls.append(sprite)
+                self.__rock_sprites.append(sprite)
+
             elif self.__agent.environment.is_log(state):
                 sprite = arcade.Sprite(":resources:images/topdown_tanks/treeBrown_large.png", 0.25)
 
@@ -39,7 +43,8 @@ class MapWindow(arcade.Window):
         self.__goal = arcade.Sprite(":resources:images/items/coinGold.png", 0.5)
         self.__goal.center_x, self.__goal.center_y = self.state_to_xy(self.__agent.environment.treasure)
 
-        self.__adventurer = arcade.Sprite(":resources:images/animated_characters/female_adventurer/femaleAdventurer_walk3.png", 0.3)
+        self.__adventurer = arcade.Sprite(
+            ":resources:images/animated_characters/female_adventurer/femaleAdventurer_walk3.png", 0.3)
         self.__adventurer.center_x, self.__adventurer.center_y = self.state_to_xy(self.__agent.state)
 
     def state_to_xy(self, state):
@@ -47,8 +52,11 @@ class MapWindow(arcade.Window):
                (self.__agent.environment.height - state[0] - 0.5) * Consts.SPRITE_SIZE
 
     def on_draw(self):
+        # TODO Voir si'il faut rajouter self.__rock_sprites.draw()
+        # Le but c'est d'enlever les rocks
         arcade.start_render()
         self.__walls.draw()
+        self.__rock_sprites.draw()
         self.__goal.draw()
         self.__adventurer.draw()
 
@@ -61,9 +69,20 @@ class MapWindow(arcade.Window):
         self.__iteration += 1
 
     def on_update(self, delta_time):
+
         if self.__agent.state != self.__agent.environment.treasure:
             action, reward = self.__agent.step()
             self.__adventurer.center_x, self.__adventurer.center_y = self.state_to_xy(self.__agent.state)
+            # Call update on all sprites (The sprites don't do much in this
+            # example though.)
+            self.__rock_sprites.update()
+
+            # Generate a list of all sprites that collided with the player.
+            hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.__rock_sprites)
+
+            # Loop through each colliding sprite, remove it, and add to the score.
+            for rock in hit_list:
+                rock.remove_from_sprite_lists()
         else:
             self.new_game()
 
